1cf373c5d40565a158fe0d3a8a48786c897b7d4c
Roland Archie <rolandarchie93@gmail.com>
2023-12-04 23:32:08 +0000

    intro_functions

---
+++ intro_functions.py
@@ -0,0 +1,9 @@
+#functions can help divide code into isolated pieces
+def greet():
+    print('Hello, my dear!')
+# functions defintion starts with def which is short for define
+#after a space, provide functions name. Any name you want
+# after name provide round brackets
+#end line with a colon
+#need an indented instruction
+greet()
