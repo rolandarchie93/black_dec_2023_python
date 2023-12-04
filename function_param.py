4d7b49291ec33e7d320e187cade396c181b0adca
Roland Archie <rolandarchie93@gmail.com>
2023-12-04 23:49:11 +0000

    function_param

---
+++ function_param.py
@@ -0,0 +1,20 @@
+input_numbers = [5.0, 3.5, 7.8,9.9, 10.0]
+
+sum = 0.0
+for number in input_numbers:
+    sum += number
+average = sum / len(input_numbers)
+
+print(average)
+
+
+input_numbers = [5.0, 3.5, 7.8,9.9, 10.0]
+
+def get_average(input_numbers):
+    sum = 0.0
+    for number in input_numbers:
+        sum += number
+    average = sum / len(input_numbers)
+    print(average)
+    
+get_average([5.0, 3.5, 7.8,9.9, 10.0])
