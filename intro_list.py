ab4faea8a2ee0161ff3d2894faa845733179d58c
Roland Archie <rolandarchie93@gmail.com>
2023-12-04 13:14:53 +0000

    intro_list

---
+++ intro_list.py
@@ -0,0 +1,18 @@
+# collections stores data types one or more value in a single variable
+#lists tuples dictionaries
+# lists store multiple value of same type
+
+empty_list = []
+top_cities = ['NYC', 'LA', 'CHI', 'HOU', 'PHX']
+print(top_cities)
+#list indicies start at 0
+#indexing: provide the index of the element inside square brackets
+top_cities[2]
+
+print(top_cities[0:2])
+top_city = 'New York City' #standard string variable, with single string inside
+top_cities= [] #empty lists
+top_cities = ['NYC', 'LA', 'CHI', 'HOU', 'PHX'] #LIST WITH MULTIPLE ELEMENTS
+top_cities[0] #access to first element
+top_cities[-5] #fifth element from the end
+top_cities[0:3] #gives you three elements
